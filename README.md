ac3f0177b77ec577c417dfa2fe9b7bb487d611b2
dopin <dopinninja@gmail.com>
2020-08-12 18:55:36 +0900

    Rust Nth Prime

---
+++ README.md
@@ -0,0 +1,4 @@
+# My Solutions for Exercism Excercises
+
+I play around in [Exercism.io](https://exercism.io/profiles/dopin) sometimes and here are my solutions.
+IOW, remnants.
---
+++ rust/nth-prime/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn nth(n: u32) -> u32 {
+    let mut primes: Vec<u32> = vec![2];
+
+    for num in 3.. {
+        if primes.len() as u32 > n {
+            break;
+        }
+        if primes.iter().any(|&p| num % p == 0) {
+            continue;
+        } else {
+            primes.push(num);
+        }
+    }
+
+    *primes.last().unwrap()
+}

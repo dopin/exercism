8f750b9ea6a42d2e47c258480542a2fe151fff01
dopin <dopinninja@gmail.com>
2020-08-13 15:10:48 +0900

    Fix Rust Bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,14 +1,23 @@
 pub fn reply(text: &str) -> &str {
-  if text == "" {
-    return "Fine. Be that way!";
-  } else if text.ends_with('?') {
-    return "Sure.";
-  }
+    if text == "" || text.chars().all(char::is_whitespace) {
+        return "Fine. Be that way!";
+    }
 
-  let lowercases: Vec<&str> = text.matches(char::is_lowercase).collect();
-  if lowercases.len() == 0 {
-    return "Whoa, chill out!";
-  }
+    if text.trim_end().ends_with('?') {
+        let question = text.chars().filter(|c| c.is_alphabetic());
+        let is_all_uppercase = question.clone().all(char::is_uppercase);
+        let char_count = question.count();
+        if is_all_uppercase && char_count > 0 {
+            return "Calm down, I know what I'm doing!";
+        } else {
+            return "Sure.";
+        }
+    }
 
-  "Whatever."
+    let alphabets = text.chars().filter(|c| c.is_alphabetic());
+    if alphabets.clone().count() > 0 && alphabets.clone().all(char::is_uppercase) {
+        return "Whoa, chill out!";
+    }
+
+    "Whatever."
 }
